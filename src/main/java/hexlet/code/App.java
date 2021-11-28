93ab4f7b4c0adc404fd0f3b0467edd1ab52150f9
Julia Trevgoda <jtrevgoda@jtrevgoda.fritz.box>
2021-11-28 15:43:48 +0100

    implement game choice menu

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,7 +1,27 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        Cli.sayHello();
+        while (true) {
+            System.out.println("Please enter the game number and press Enter." +
+                    "\n1 - Greet" +
+                    "\n2 - Even" +
+                    "\n0 - Exit");
+
+            Scanner scanner = new Scanner(System.in);
+            int gameChoice = scanner.nextInt();
+            String currentChoice = "Your choice: " + gameChoice;
+            System.out.println(currentChoice);
+
+            switch (gameChoice) {
+                case (1):
+                    Cli.sayHello();
+                    break;
+                case (0):
+                    System.exit(0);
+            }
+        }
     }
 }
