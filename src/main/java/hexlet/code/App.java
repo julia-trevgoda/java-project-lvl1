a61a224c1d8f4f99f7fcec81f1e0eef5388551be
Julia Trevgoda <julia.trevgoda@gmail.com>
2021-11-30 08:24:37 +0100

    trying fix tests

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -33,10 +33,11 @@ public class App {
                 userName = Cli.sayHello();
             } else if (gameChoice == 0) {
                 System.exit(0);
-            } else if (userName == null && gameChoice > 1) {
-                userName = Cli.sayHello();
-                Engine.playGame(gameChoice);
+//            } else if (userName == null && gameChoice > 1) {
+//                userName = Cli.sayHello();
+//                Engine.playGame(gameChoice);
             } else {
+                userName = Cli.sayHello();
                 Engine.playGame(gameChoice);
             }
         }
