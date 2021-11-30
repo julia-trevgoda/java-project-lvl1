a2a0203d9d97c8ae4b44cc2f66bc5a228dea42ec
Julia Trevgoda <julia.trevgoda@gmail.com>
2021-11-30 08:43:25 +0100

    fix indentations

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -12,34 +12,34 @@ public class App {
 
     public static void main(String[] args) {
 //        while (true) {
-            System.out.println("Please enter the game number and press Enter."
-                    + "\n1 - Greet"
-                    + "\n2 - Even"
-                    + "\n3 - Calc"
-                    + "\n4 - GCD"
-                    + "\n5 - Progression"
-                    + "\n6 - Prime"
-                    + "\n0 - Exit");
+        System.out.println("Please enter the game number and press Enter."
+                + "\n1 - Greet"
+                + "\n2 - Even"
+                + "\n3 - Calc"
+                + "\n4 - GCD"
+                + "\n5 - Progression"
+                + "\n6 - Prime"
+                + "\n0 - Exit");
 
-            Scanner scanner = new Scanner(System.in);
-            int gameChoice = scanner.nextInt();
+        Scanner scanner = new Scanner(System.in);
+        int gameChoice = scanner.nextInt();
 
-            String currentChoice = "Your choice: " + gameChoice;
-            System.out.println(currentChoice);
-            System.out.println();
+        String currentChoice = "Your choice: " + gameChoice;
+        System.out.println(currentChoice);
+        System.out.println();
 
 
-            if (gameChoice == 1) {
-                userName = Cli.sayHello();
-            } else if (gameChoice == 0) {
-                System.exit(0);
+        if (gameChoice == 1) {
+            userName = Cli.sayHello();
+        } else if (gameChoice == 0) {
+            System.exit(0);
 //            } else if (userName == null && gameChoice > 1) {
 //                userName = Cli.sayHello();
 //                Engine.playGame(gameChoice);
-            } else {
-                userName = Cli.sayHello();
-                Engine.playGame(gameChoice);
-            }
+        } else {
+            userName = Cli.sayHello();
+            Engine.playGame(gameChoice);
         }
+    }
 //    }
 }
